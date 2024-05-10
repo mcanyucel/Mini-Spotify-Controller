cce06d0569af11ef57d6f18d2e565ce30f5ec29f
Mustafa Can Yucel <m.can.yucel.1@gmail.com>
2024-05-10 15:45:53 +0300

    progress

--- model/PlaybackState.cs
+++ model/PlaybackState.cs
@@ -2,42 +2,41 @@
 
 namespace MiniSpotifyController.model;
 
-public class PlaybackState : ObservableObject
+public sealed partial class PlaybackState : ObservableObject
 {
-    public bool IsPlaying { get => m_IsPlaying; set => SetProperty(ref m_IsPlaying, value); }
-    public string? CurrentlyPlayingId { get => m_CurrentlyPlayingId; set => SetProperty(ref m_CurrentlyPlayingId, value); }
-    public string? CurrentlyPlayingSpotifyId { get => m_CurrentlyPlayingSpotifyId; set => SetProperty(ref m_CurrentlyPlayingSpotifyId, value); }
-    public string? CurrentlyPlaying { get => m_CurrentlyPlaying; set => SetProperty(ref m_CurrentlyPlaying, value); }
-    public string? CurrentlyPlayingArtist { get => m_CurrentlyPlayingArtist; set => SetProperty(ref m_CurrentlyPlayingArtist, value); }
-    public Album? CurrentlyPlayingAlbum { get => m_CurrentlyPlayingAlbum; set => SetProperty(ref m_CurrentlyPlayingAlbum, value); }
-    public string? DeviceId { get => m_DeviceId; set => SetProperty(ref m_DeviceId, value); }
-    public int ProgressMs { get => m_ProgressMs; private set => SetProperty(ref m_ProgressMs, value); }
-    public int DurationMs { get => m_DurationMs; set => SetProperty(ref m_DurationMs, value); }
-    public bool IsLiked { get => m_IsLiked; set => SetProperty(ref m_IsLiked, value); }
-    public bool IsBusy { get => m_IsBusy; set => SetProperty(ref m_IsBusy, value); }
+
+    [ObservableProperty]
+    bool isPlaying;
+    [ObservableProperty]
+    string? currentlyPlayingId;
+    [ObservableProperty]
+    string? currentlyPlayingSpotifyId;
+    [ObservableProperty]
+    string? currentlyPlaying;
+    [ObservableProperty]
+    string? currentlyPlayingArtist;
+    [ObservableProperty]
+    Album? currentlyPlayingAlbum;
+    [ObservableProperty]
+    string? deviceId;
+    [ObservableProperty]
+    int progressMs;
+    [ObservableProperty]
+    int durationMs;
+    [ObservableProperty]
+    bool isLiked;
+    [ObservableProperty]
+    bool isBusy;
     public void IncrementProgress(int delta, bool isSeeking)
     {
 
         if (!isSeeking)
             ProgressMs += delta;
         else
-            m_ProgressMs += delta;
+#pragma warning disable MVVMTK0034 // Direct field reference to [ObservableProperty] backing field
+            progressMs += delta;
+#pragma warning restore MVVMTK0034 // Direct field reference to [ObservableProperty] backing field
     }
     public void ResetProgress() =>  ProgressMs = 0;
     public void SetProgress(int progress) => ProgressMs = progress;
-
-    #region Fields
-
-    private bool m_IsPlaying;
-    private bool m_IsLiked;
-    private string? m_CurrentlyPlaying;
-    private string? m_CurrentlyPlayingArtist;
-    private Album? m_CurrentlyPlayingAlbum;
-    private string? m_DeviceId;
-    private int m_ProgressMs;
-    private int m_DurationMs;
-    private string? m_CurrentlyPlayingId;
-    private bool m_IsBusy;
-    private string? m_CurrentlyPlayingSpotifyId;
-    #endregion
 }
